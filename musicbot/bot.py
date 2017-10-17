4a3589e9fa980e77d208bfa5323bb9295fc40368
Blue <simon@catbus.co.uk>
2017-10-17 02:11:15 +0100

    Fix bug

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -2118,7 +2118,7 @@ class MusicBot(discord.Client):
         self.should_restart |= result.stdout != b'Already up-to-date.\n'
         if self.should_restart:
             max_time = timedelta(seconds=0)
-            for player in self.players.items():
+            for player in self.players.values():
                 playlist = player.playlist
                 max_time = max((max_time, await playlist.estimate_time_until(len(playlist.entries), player)))
             description = f"Scheduled a restart after the queue is empty, restart estimated to be in {max_time}."
